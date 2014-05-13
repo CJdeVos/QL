52b83980788553785375a6cb8df62175e042c5fc
CJ de Vos <cj.de.vos@cxo-cockpit.com>
2014-05-13 17:33:54 +0200

    Minor additions basic setup

---
+++ QL.Core/Data.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL.Core
+{
+    public class Data
+    {
+
+
+        public Cell this[int row, int column]
+        {
+            get
+            {
+                return new Cell();
+            }
+        }
+
+    }
+}
