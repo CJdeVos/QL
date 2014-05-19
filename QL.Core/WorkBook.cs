1b897c913a0feb389924febc04e2de5d6784a491
CJ de Vos <cj.de.vos@cxo-cockpit.com>
2014-05-19 08:29:29 +0200

    c

---
+++ QL.Core/WorkBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL.Core
+{
+    public class WorkBook
+    {
+        private readonly WorkSheets __sheets = new WorkSheets();
+
+        public WorkSheets WorkSheets
+        {
+            get
+            {
+                return __sheets;
+            }
+        }
+
+
+        public static WorkBook Create()
+        {
+            return new WorkBook();
+        }
+    }
+}
---
+++ QL.Core/WorkSheet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL.Core
+{
+    public class WorkSheet
+    {
+        internal WorkSheet()
+        {
+            Data = new Data();
+        }
+
+
+        /*public Range Cells
+        {
+            get
+            {
+                return new Range();
+            }
+        }*/
+
+        internal Data Data
+        {
+            get;
+            private set;
+        }
+
+        public Range GetRange(string selector)
+        {
+            return Range.FromString(this, selector);
+            // parse selector to return range
+            //return new Range();
+        }
+    }
+}
